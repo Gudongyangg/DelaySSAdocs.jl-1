06f5b4109e3017e4b8f4a423c6e6463190233e61
Documenter.jl <documenter@juliadocs.github.io>
2021-12-01 09:57:25 +0000

    build based on 903c12b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DelaySSAdocs","category":"page"},{"location":"#DelaySSAdocs","page":"Home","title":"DelaySSAdocs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DelaySSAdocs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DelaySSAdocs]","category":"page"}]
+}
