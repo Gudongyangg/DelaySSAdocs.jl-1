92fc9b9cad6d6acd5d326ab1da6e496b6fbbe216
xiaoming <palmtree2013@163.com>
2021-12-24 15:05:29 +0800

    api

--- docs/src/api.md
+++ docs/src/api.md
@@ -8,6 +8,15 @@ DelayJumpProblem
 DelayJumpSet
 ```
 
-```@docs
-delay_trigger
-```
+## Define `DelayJumpSet`
+Further notes on `delay_trigger`, `delay_interrupt`, `delay_complete`.
+### `delay_trigger` 
+   delay_trigger defines a `Dict` type:
+
+- Keys: Indices of reactions defined in the Markovian part that can trigger the delay reaction;
+- Values: An update function or a `Pair` type that determines how to update the delay channel.
+ 
+
+### `delay_interrupt`
+ 
+### `delay_complete` 
--- src/doc_funcs.jl
+++ src/doc_funcs.jl
@@ -7,15 +7,15 @@ A delay jump set...
 $(FIELDS)
 
 # Notes
--`delay_trigger::Dict{Int,Any}`     #  those reactions in the Markovian part that trigger the change of the state of the delay channels or/and the state of the reactants upon initiation. Keys: Indices of reactions defined in the Markovian part that can trigger the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+- `delay_trigger::Dict{Int,Any}`     #  those reactions in the Markovian part that trigger the change of the state of the delay channels or/and the state of the reactants upon initiation. Keys: Indices of reactions defined in the Markovian part that can trigger the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
 
--`delay_interrupt::Dict{Int,Any}`   # those reactions in the Markovian part that change the state of the delay channels or/and the state of the reactants in the middle of on-going delay reactions. Keys: Indices of reactions defined in the Markovian part that can interrupt the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+- `delay_interrupt::Dict{Int,Any}`   # those reactions in the Markovian part that change the state of the delay channels or/and the state of the reactants in the middle of on-going delay reactions. Keys: Indices of reactions defined in the Markovian part that can interrupt the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
 
--`delay_complete::Dict{Int,Any}`    # those reactions that are initiated by delay trigger reactions and change the state of the delay channels or/and the state of the reactants upon completion. Keys: Indices of the delay channel; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+- `delay_complete::Dict{Int,Any}`    # those reactions that are initiated by delay trigger reactions and change the state of the delay channels or/and the state of the reactants upon completion. Keys: Indices of the delay channel; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
 
--`delay_trigger_set::Vector{Int}`   # collect(keys(delay_trigger))
+- `delay_trigger_set::Vector{Int}`   # collect(keys(delay_trigger))
 
--`delay_interrupt_set::Vector{Int}` # collect(keys(delay_interrupt))
+- `delay_interrupt_set::Vector{Int}` # collect(keys(delay_interrupt))
 """
 mutable struct DelayJumpSet
     """those reactions in the Markovian part that trigger the change of the state of the delay channels or/and the state of the reactants upon initiation."""
@@ -27,28 +27,22 @@ mutable struct DelayJumpSet
     """collect(keys(delay_trigger))"""
     delay_trigger_set::Vector{Int}
     """collect(keys(delay_interrupt))"""
-    delay_interrupt_set::Vector{Int}
+    delay_interrupt_set::Vector{Int}    
 end
 
-"""
-    function(delay_trigger,delay_complete,delay_interrupt)
-
-The input arguments are
-
--`delay_trigger::Dict{Int,Any}`     #  those reactions in the Markovian part that trigger the change of the state of the delay channels or/and the state of the reactants upon initiation. Keys: Indices of reactions defined in the Markovian part that can trigger the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
-
--`delay_interrupt::Dict{Int,Any}`   # those reactions in the Markovian part that change the state of the delay channels or/and the state of the reactants in the middle of on-going delay reactions. Keys: Indices of reactions defined in the Markovian part that can interrupt the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
-
--`delay_complete::Dict{Int,Any}`    # those reactions that are initiated by delay trigger reactions and change the state of the delay channels or/and the state of the reactants upon completion. Keys: Indices of the delay channel; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
-"""
 DelayJumpSet(delay_trigger,delay_complete,delay_interrupt) = DelayJumpSet(delay_trigger,delay_complete,delay_interrupt, collect(keys(delay_trigger)), collect(keys(delay_interrupt)))
+
 
 
 
 """
     function DelayJumpProblem(prob::DiscreteProblem, aggregator::AbstractDelayAggregatorAlgorithm, jumps::JumpSet, delayjumpsets::DelayJumpSet, de_chan0)
-
+# Fields
+- `prob::DiscreteProblem`
+- `aggregator::AbstractDelayAggregatorAlgorithm`
 - `jumps::JumpSet`
+- `delayjumpsets::DelayJumpSet`
+- `de_chan0::Vector{Vector{T}}` 
         
 """
 function DelayJumpProblem(prob, aggregator, jumps, delayjumpsets::DelayJumpSet, de_chan0;
@@ -102,11 +96,12 @@ end
 
 """
     function DelayJumpProblem(js::JumpSystem, prob, aggregator, delayjumpset, de_chan0; kwargs...)
-
-#Arguments
+# Fields
 - `js::JumpSystem`: ...
 - `prob::DiscreteProblem`: ...
-- `aggregator::` ...
+- `aggregator::AbstractDelayAggregatorAlgorithm`
+- `delayjumpsets::DelayJumpSet`
+- `de_chan0::Vector{Vector{T}}` 
 """
 function DelayJumpProblem(js, prob, aggregator, delayjumpset, de_chan0; kwargs...)
     statetoid = Dict(value(state) => i for (i,state) in enumerate(states(js)))
@@ -135,23 +130,4 @@ function DelayJumpProblem(js, prob, aggregator, delayjumpset, de_chan0; kwargs..
     DelayJumpProblem(prob, aggregator, jset, delayjumpset, de_chan0; dep_graph=jtoj, vartojumps_map=vtoj, jumptovars_map=jtov, scale_rates=false, nocopy=true, kwargs...)
 end
 
-"""
-    delay_trigger defines a `Dict` type:
-
-- Keys: Indices of reactions defined in the Markovian part that can trigger the delay reaction;
-- Values: An update function or a `Pair` type that determines how to update the delay channel.
-
-# Notes
-- 
-"""
-function delay_trigger end
-
-"""
-
-"""
-function delay_interrupt end
-
-"""
-
-"""
-function delay_complete end
+ 
