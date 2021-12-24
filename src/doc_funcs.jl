6cd8857c81850aacfcfcf30bd83c34a517e3345a
xiaoming <palmtree2013@163.com>
2021-12-24 15:10:57 +0800

    Update doc_funcs.jl

--- src/doc_funcs.jl
+++ src/doc_funcs.jl
@@ -7,11 +7,17 @@ A delay jump set...
 $(FIELDS)
 
 # Notes
-- `delay_trigger::Dict{Int,Any}`     #  those reactions in the Markovian part that trigger the change of the state of the delay channels or/and the state of the reactants upon initiation. Keys: Indices of reactions defined in the Markovian part that can trigger the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+- `delay_trigger::Dict{Int,Any}`     #  those reactions in the Markovian part that trigger the change of the state of the delay channels or/and the state of the reactants upon initiation. 
+    - Keys: Indices of reactions defined in the Markovian part that can trigger the delay reactions; 
+    - Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
 
-- `delay_interrupt::Dict{Int,Any}`   # those reactions in the Markovian part that change the state of the delay channels or/and the state of the reactants in the middle of on-going delay reactions. Keys: Indices of reactions defined in the Markovian part that can interrupt the delay reactions; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+- `delay_interrupt::Dict{Int,Any}`   # those reactions in the Markovian part that change the state of the delay channels or/and the state of the reactants in the middle of on-going delay reactions. 
+    - Keys: Indices of reactions defined in the Markovian part that can interrupt the delay reactions; 
+    - Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
 
-- `delay_complete::Dict{Int,Any}`    # those reactions that are initiated by delay trigger reactions and change the state of the delay channels or/and the state of the reactants upon completion. Keys: Indices of the delay channel; Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
+- `delay_complete::Dict{Int,Any}`    # those reactions that are initiated by delay trigger reactions and change the state of the delay channels or/and the state of the reactants upon completion. 
+    - Keys: Indices of the delay channel; 
+    - Values: Update functions(or `Pair` type) that decide how to update the delay channel or the state of the reactants.
 
 - `delay_trigger_set::Vector{Int}`   # collect(keys(delay_trigger))
 
